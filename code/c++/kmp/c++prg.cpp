ee78523d7a851733ee52fce42d0352777a75188a
QianMaverick <schmieladamon7f@gmail.com>
2026-05-28 19:05:22 +0800

    Create c++prg.cpp

---
+++ code/c++/kmp/c++prg.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string p{},s{};
+    cin >> p >> s;
+    vector<int> nexts(p.size(),{}),ans{};
+    for(int i=1,j=0;i<p.size();++i)
+    {
+        while(j!=0&&p[i]!=p[j])
+        {
+            j=nexts[j-1];
+        }
+        if(p[i]==p[j])
+        {
+            ++j;
+        }
+        nexts[i]=j;
+    }
+    for(int i=0,j=0;i<s.size();++i)
+    {
+        while(j!=0&&s[i]!=p[j])
+        {
+            j=nexts[j-1];
+        }
+        if(s[i]==p[j])
+        {
+            ++j;
+        }
+        if(j==p.size())
+        {
+            ans.emplace_back(i-p.size()+1);
+            j=nexts[j-1];
+        }
+    }
+    for(int a:ans)
+    {
+        cout << a << " ";
+    }
+    cout << endl;
+    return;
+}
+
+signed main()
+{
+    solve();
+    return 0;
+}
