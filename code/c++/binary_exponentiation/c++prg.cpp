40be56dfcd36d5ee4ebce5a58ea47e984a0da4f6
QianMaverick <schmieladamon7f@gmail.com>
2026-06-02 16:26:00 +0800

    Create c++prg.cpp

---
+++ code/c++/binary_exponentiation/c++prg.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_exponentiation(int a,int b)
+{
+    int num=1;
+    while(b!=0)
+    {
+        if(b%2==1)
+        {
+            num=num*a;
+        }
+        a=a*a;
+        b=b/2;
+    }
+    return num;
+}
+
+void solve()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << binary_exponentiation(a,b) << endl;
+    return;
+}
+
+signed main()
+{
+    solve();
+    return 0;
+}
