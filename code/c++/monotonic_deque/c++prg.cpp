14cffcf3602a2265b239e4e2df95f686d0ee0474
QianMaverick <schmieladamon7f@gmail.com>
2026-06-12 14:30:38 +0800

    Create c++prg.cpp

---
+++ code/c++/monotonic_deque/c++prg.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<int> nums(n);
+    for(int i=0;i<n;++i)
+    {
+        cin >> nums[i];
+    }
+    deque<int> dq;
+    for(int i=0;i<n;++i)
+    {
+        while(!dq.empty()&&nums[dq.back()]>nums[i])
+        {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        while(dq.front()<=i-m)
+        {
+            dq.pop_front();
+        }
+        if(i>=m-1)
+        {
+            cout << nums[dq.front()] << " ";
+        }
+    }
+    cout << endl;
+    return;
+}
+
+signed main()
+{
+    solve();
+    return 0;
+}
