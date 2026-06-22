7011cc457121be6476d662c863ce47fca77f7fd8
QianMaverick <schmieladamon7f@gmail.com>
2026-06-22 22:27:11 +0800

    Update c++prg.cpp

--- code/c++/adjacency_matrix_graph/c++prg.cpp
+++ code/c++/adjacency_matrix_graph/c++prg.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int inf=numeric_limits<int>::max()/2;
 
-void bfs(vector<vector<int>> graph,vector<bool>& visited,int start,vector<int>& ans)
+void bfs(vector<vector<int>> graph,vector<bool> visited,int start,vector<int>& ans)
 {
     queue<int> q;
     q.emplace(start);
@@ -24,7 +24,7 @@ void bfs(vector<vector<int>> graph,vector<bool>& visited,int start,vector<int>& 
     return;
 }
 
-void dfs(vector<vector<int>> graph,vector<bool>& visited,int start,vector<int>& ans)
+void dfs(vector<vector<int>> graph,vector<bool> visited,int start,vector<int>& ans)
 {
     visited[start]=true;
     ans.emplace_back(start);
@@ -47,9 +47,9 @@ void solve()
     for(int i=0;i<m;++i)
     {
         int u,v,w;
-        cin >> u >> v >> w; //
+        cin >> u >> v >> w;
         graph[u][v]=w;
-        graph[v][u]=w; //
+        graph[v][u]=w;
     }
     int start=0;
     vector<int> ans;
