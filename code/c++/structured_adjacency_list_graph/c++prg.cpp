646b183020c41fdc3844a4ece4d4b7dc050e019f
QianMaverick <schmieladamon7f@gmail.com>
2026-06-22 22:27:13 +0800

    Update c++prg.cpp

--- code/c++/structured_adjacency_list_graph/c++prg.cpp
+++ code/c++/structured_adjacency_list_graph/c++prg.cpp
@@ -1,70 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct edge
+class edge
 {
-    int to{};
-    int weight{};
-    edge():to(0),weight(0){}
-    edge(int& t):to(t),weight(){}
-    edge(int& t,int& w):to(t),weight(w){}
+public:
+    int v;
+    int w;
+    edge(int v,int w):v(v),w(w){}
 };
 
-void bfs(vector<vector<edge>>& graph,vector<bool>& visited,int& start,vector<int>& ans)
+void bfs(vector<vector<edge>> graph,vector<bool> visited,int start,vector<int>& ans)
 {
-    queue<int> q{};
+    queue<int> q;
     q.emplace(start);
     visited[start]=true;
     while(!q.empty())
     {
-        int cur{q.front()};
+        int cur=q.front();
         q.pop();
         ans.emplace_back(cur);
-        for(auto& [to,weight]:graph[cur])
+        for(edge nex:graph[cur])
         {
-            if(!visited[to])
+            if(!visited[nex.v])
             {
-                q.emplace(to);
-                visited[to]=true;
+                q.emplace(nex.v);
+                visited[nex.v]=true;
             }
         }
     }
     return;
 }
 
-void bfsall(vector<vector<edge>>& graph,vector<bool>& visited,vector<int>& ans)
+void dfs(vector<vector<edge>> graph,vector<bool> visited,int start,vector<int>& ans)
 {
-    for(int i=0;i<graph.size();++i)
-    {
-        if(!visited[i]&&!graph[i].empty()) //
-        {
-            bfs(graph,visited,i,ans);
-        }
-    }
-    return;
-}
-
-void dfs(vector<vector<edge>>& graph,vector<bool>& visited,int& start,vector<int>& ans)
-{
-    ans.emplace_back(start);
     visited[start]=true;
-    for(auto& [to,weight]:graph[start])
+    ans.emplace_back(start);
+    for(edge nex:graph[start])
     {
-        if(!visited[to])
+        if(!visited[nex.v])
         {
-            dfs(graph,visited,to,ans);
-        }
-    }
-    return;
-}
-
-void dfsall(vector<vector<edge>>& graph,vector<bool>& visited,vector<int>& ans)
-{
-    for(int i=0;i<graph.size();++i)
-    {
-        if(!visited[i]&&!graph[i].empty()) //
-        {
-            dfs(graph,visited,i,ans);
+            dfs(graph,visited,nex.v,ans);
         }
     }
     return;
@@ -72,26 +47,24 @@ void dfsall(vector<vector<edge>>& graph,vector<bool>& visited,vector<int>& ans)
 
 void solve()
 {
-    int nodes{},edges{};
-    cin >> nodes >> edges;
-    vector<vector<edge>> graph(nodes,vector<edge>{});
-    vector<bool> visited(nodes,false);
-    vector<int> ans{};
-    int start{0};
-    for(int i=0;i<edges;++i)
+    int n,m;
+    cin >> n >> m;
+    vector<vector<edge>> graph(n);
+    vector<bool> visited(n,false);
+    for(int i=0;i<m;++i)
     {
-        int from{},to{},weight{};
-        cin >> from >> to >> weight; //
-        graph[from].emplace_back(to,weight);
-        graph[to].emplace_back(from,weight); //
+        int u,v,w;
+        cin >> u >> v >> w;
+        graph[u].emplace_back(v,w);
+        graph[v].emplace_back(u,w);
     }
+    int start=0;
+    vector<int> ans;
     bfs(graph,visited,start,ans);
-    // bfsall(graph,visited,ans);
-    // dfs(graph,visited,start,ans);
-    // dfsall(graph,visited,ans);
-    for(int& a:ans)
+    dfs(graph,visited,start,ans);
+    for(int i=0;i<ans.size();++i)
     {
-        cout << a << " ";
+        cout << ans[i] << " ";
     }
     cout << endl;
     return;
