afda10bd143932dfcfe1768c7d96981936aacd9e
QianMaverick <schmieladamon7f@gmail.com>
2026-06-23 10:26:27 +0800

    Update c++prg.cpp

--- code/c++/random_number/c++prg.cpp
+++ code/c++/random_number/c++prg.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 void solve()
 {
-    int min_limit{},max_limit{};
-    cin >> min_limit >> max_limit;
-    static random_device seed{};
+    int a,b;
+    cin >> a >> b;
+    static random_device seed;
     static mt19937 engine(seed());
-    uniform_int_distribution<int> scope(min_limit,max_limit); // [1,100]
-    // uniform_real_distribution<double> scope(min_limit,max_limit); // [1,100)
-    int num{scope(engine)};
-    // double num{scope(engine)};
+    uniform_int_distribution<int> scope(a,b); // [a,b]
+    int num=scope(engine);
+    cout << num << endl;
+    uniform_real_distribution<double> scope(a,b); // [a,b)
+    double num=scope(engine);
     cout << num << endl;
     return;
 }
