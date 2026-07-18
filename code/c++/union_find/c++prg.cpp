1a18ce08e39988c89b7854669c92b57feaf051df
QianMaverick <schmieladamon7f@gmail.com>
2026-07-18 16:30:01 +0800

    Update c++prg.cpp

--- code/c++/union_find/c++prg.cpp
+++ code/c++/union_find/c++prg.cpp
@@ -1,62 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class union_find
+int find(int x,vector<int>& fa)
 {
-public:
-    vector<int> fa;
-    vector<int> sz;
-    union_find(int n)
+    int root=x;
+    while(root!=fa[root])
     {
-        fa.resize(n,0);
-        sz.resize(n,1);
-        for(int i=0;i<n;++i)
-        {
-            fa[i]=i;
-        }
+        root=fa[root];
     }
-    int find(int x)
+    while(fa[x]!=root)
     {
-        int root=x;
-        while(root!=fa[root])
-        {
-            root=fa[root];
-        }
-        while(fa[x]!=root)
-        {
-            int temp{fa[x]};
-            fa[x]=root;
-            x=temp;
-        }
-        return root;
+        int temp=fa[x];
+        fa[x]=root;
+        x=temp;
     }
-    bool merge(int x,int y)
+    return root;
+}
+
+bool merge(int x,int y,vector<int>& fa,vector<int>& sz)
+{
+    x=find(x,fa);
+    y=find(y,fa);
+    if(x==y)
     {
-        x=find(x);
-        y=find(y);
-        if(x==y)
-        {
-            return false;
-        }
-        if(sz[x]<sz[y])
-        {
-            swap(x,y);
-        }
-        fa[y]=x;
-        sz[x]=sz[x]+sz[y];
-        return true;
+        return false;
     }
-};
+    if(sz[x]<sz[y])
+    {
+        swap(x,y);
+    }
+    fa[y]=x;
+    sz[x]=sz[x]+sz[y];
+    return true;
+}
 
 void solve()
 {
     int n;
     cin >> n;
-    union_find uf(n);
+    vector<int> fa(n,0);
+    vector<int> sz(n,1);
+    for(int i=0;i<n;++i)
+    {
+        fa[i]=i;
+    }
     int a,b;
     cin >> a >> b;
-    uf.find(a);
-    uf.merge(a,b);
+    find(a,fa);
+    find(b,fa);
+    merge(a,b,fa,sz);
     return;
 }
 
