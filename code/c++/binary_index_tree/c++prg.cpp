474f254f938ff08bcf11172c7e1f57224eaaf534
QianMaverick <schmieladamon7f@gmail.com>
2026-07-22 12:15:18 +0800

    Create c++prg.cpp

---
+++ code/c++/binary_index_tree/c++prg.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowbit(int x)
+{
+    return (x&-x);
+}
+
+void add(int i,int num,vector<int>& nums)
+{
+    int n=nums.size();
+    for(;i<=n;i=i+lowbit(i))
+    {
+        nums[i]=nums[i]+num;
+    }
+    return;
+}
+
+int sum(int i,vector<int>& nums)
+{
+    int sum=0;
+    for(;i>=1;i=i-lowbit(i))
+    {
+        sum=sum+nums[i];
+    }
+    return sum;
+}
+
+void solve()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<int> nums(n+1,0);
+    for(int i=1;i<=n;++i)
+    {
+        int num;
+        cin >> num;
+        add(i,num,nums);
+    }
+    while(m--)
+    {
+        int op;
+        cin >> op;
+        if(op==1)
+        {
+            int i,num;
+            cin >> i >> num;
+            add(i,num,nums);
+        }
+        else if(op==2)
+        {
+            int l,r;
+            cin >> l >> r;
+            cout << sum(r,nums)-sum(l-1,nums) << endl;
+        }
+    }
+    return;
+}
+
+signed main()
+{
+    solve();
+    return 0;
+}
