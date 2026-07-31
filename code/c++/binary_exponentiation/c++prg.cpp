3da4ed289d02d1d50b870b66644b118edcb4d7b5
QianMaverick <schmieladamon7f@gmail.com>
2026-07-31 19:50:29 +0800

    Update c++prg.cpp

--- code/c++/binary_exponentiation/c++prg.cpp
+++ code/c++/binary_exponentiation/c++prg.cpp
@@ -20,9 +20,7 @@ void solve()
 {
     int a,b;
     cin >> a >> b;
-    int num;
-    num=binary_exponentiation(a,b);
-    cout << num << endl;
+    cout << binary_exponentiation(a,b) << endl;
     return;
 }
 
