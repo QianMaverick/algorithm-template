7101320144dec88b2a86c88136835999dc64ab42
QianMaverick <schmieladamon7f@gmail.com>
2026-07-31 18:48:53 +0800

    Update longest_subarray_without_duplicates.cpp

--- code/c++/two_pointers/longest_subarray_without_duplicates.cpp
+++ code/c++/two_pointers/longest_subarray_without_duplicates.cpp
@@ -3,30 +3,30 @@ using namespace std;
 
 void solve()
 {
-	int n;
+    int n;
     cin >> n;
-	vector<int> nums(n);
-	for(int& i:nums)
+    vector<int> nums(n);
+    for(int& i:nums)
     {
         cin >> i;
     }
     int ans=0;
-	map<int,int> count;
-	for(int i=0,j=0;j<n;++j)
-	{
-		++count[nums[j]];
-		while(count[nums[j]]>1)
-		{
-			--count[nums[i++]];
-		}
-		ans=max(ans,j-i+1);
-	}
-	cout << ans << endl;
-	return;
+    map<int,int> count;
+    for(int i=0,j=0;j<n;++j)
+    {
+        ++count[nums[j]];
+        while(count[nums[j]]>1)
+        {
+            --count[nums[i++]];
+        }
+        ans=max(ans,j-i+1);
+    }
+    cout << ans << endl;
+    return;
 }
 
 signed main()
 {
-	solve();
-	return 0;
+    solve();
+    return 0;
 }
