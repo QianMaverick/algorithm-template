27181d5387f2e5a796c963566624fca9f0ba6e90
QianMaverick <schmieladamon7f@gmail.com>
2026-07-31 18:11:12 +0800

    Update c++prg.cpp

--- code/c++/dynamic_programming/longest_common_subsequence/c++prg.cpp
+++ code/c++/dynamic_programming/longest_common_subsequence/c++prg.cpp
@@ -10,9 +10,9 @@ void solve()
     vector<vector<int>> nums(n+1,vector<int>(m+1,0));
     for(int i=1;i<=n;++i)
     {
-        for(int j=1;j<=n;++j)
+        for(int j=1;j<=m;++j)
         {
-            if(a[i]==b[j])
+            if(a[i-1]==b[j-1])
             {
                 nums[i][j]=nums[i-1][j-1]+1;
             }
