506c9949da5570a6175818ac69d4373495e55340
QianMaverick <schmieladamon7f@gmail.com>
2026-08-01 17:01:48 +0800

    Update c++prg.cpp

--- code/c++/adjacency_matrix_graph/c++prg.cpp
+++ code/c++/adjacency_matrix_graph/c++prg.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int inf=numeric_limits<int>::max()/2;
 
-void bfs(vector<vector<int>> graph,vector<bool>& visited,int start,vector<int>& ans)
+void bfs(vector<vector<int>> graph,vector<bool>& visit,int start,vector<int>& ans)
 {
     queue<int> q;
     q.emplace(start);
-    visited[start]=true;
+    visit[start]=true;
     while(!q.empty())
     {
         int cur=q.front();
@@ -14,25 +14,25 @@ void bfs(vector<vector<int>> graph,vector<bool>& visited,int start,vector<int>& 
         ans.emplace_back(cur);
         for(int nex:graph[cur])
         {
-            if(!visited[nex]&&graph[cur][nex]!=inf)
+            if(!visit[nex]&&graph[cur][nex]!=inf)
             {
                 q.emplace(nex);
-                visited[nex]=true;
+                visit[nex]=true;
             }
         }
     }
     return;
 }
 
-void dfs(vector<vector<int>> graph,vector<bool>& visited,int start,vector<int>& ans)
+void dfs(vector<vector<int>> graph,vector<bool>& visit,int start,vector<int>& ans)
 {
-    visited[start]=true;
+    visit[start]=true;
     ans.emplace_back(start);
     for(int nex:graph[start])
     {
-        if(!visited[nex]&&graph[start][nex]!=inf)
+        if(!visit[nex]&&graph[start][nex]!=inf)
         {
-            dfs(graph,visited,nex,ans);
+            dfs(graph,visit,nex,ans);
         }
     }
     return;
@@ -43,7 +43,7 @@ void solve()
     int n,m;
     cin >> n >> m;
     vector<vector<int>> graph(n,vector<int>(n,inf));
-    vector<bool> visited(n,false);
+    vector<bool> visit(n,false);
     for(int i=0;i<m;++i)
     {
         int u,v,w;
@@ -51,10 +51,11 @@ void solve()
         graph[u][v]=w;
         graph[v][u]=w;
     }
-    int start=0;
+    int start;
+    cin >> start;
     vector<int> ans;
-    bfs(graph,visited,start,ans);
-    dfs(graph,visited,start,ans);
+    bfs(graph,visit,start,ans);
+    dfs(graph,visit,start,ans);
     for(int i:ans)
     {
         cout << i << " ";
