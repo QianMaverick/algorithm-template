4a9aab25b0ff9406ab46063895058e79d7ff9196
QianMaverick <schmieladamon7f@gmail.com>
2026-08-05 11:09:23 +0800

    Update c++prg.cpp

--- code/c++/prefix_difference/c++prg.cpp
+++ code/c++/prefix_difference/c++prg.cpp
@@ -1,12 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void difference()
+void prefix1()
 {
-    int n{},m{};
+    int n;
+    cin >> n;
+    vector<int> nums(n+1,0);
+    vector<int> sums(n+1,0);
+    for(int i=1;i<=n;++i)
+    {
+        cin >> nums[i];
+        sums[i]=sums[i-1]+nums[i];
+    }
+    int l,r;
+    cin >> l >> r;
+    cout << sums[r]-sums[l-1] << endl;
+    return;
+}
+
+void prefix2()
+{
+    int n,m;
     cin >> n >> m;
-    int sums[n+2][m+2]{};
-    int nums[n+2][m+2]{};
+    vector<vector<int>> nums(n+1,vector<int>(m+1,0));
+    vector<vector<int>> sums(n+1,vector<int>(m+1,0));
+    for(int i=1;i<=n;++i)
+    {
+        for(int j=1;j<=m;++j)
+        {
+            cin >> nums[i][j];
+            sums[i][j]=nums[i][j]+sums[i-1][j]+sums[i][j-1]-sums[i-1][j-1];
+        }
+    }
+    int x1,y1,x2,y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << sums[x2][y2]-sums[x1-1][y2]-sums[x2][y1-1]+sums[x1-1][y1-1] << endl;
+    return;
+}
+
+void difference1()
+{
+    int n;
+    cin >> n;
+    vector<int> sums(n+2,0);
+    vector<int> nums(n+2,0);
+    for(int i=1;i<=n;++i)
+    {
+        cin >> sums[i];
+        nums[i]=sums[i]-sums[i-1];
+    }
+    int l,r,num;
+    cin >> l >> r >> num;
+    nums[l]=nums[l]+num;
+    nums[r+1]=nums[r+1]-num;
+    for(int i=1;i<=n;++i)
+    {
+        sums[i]=sums[i-1]+nums[i];
+        cout << sums[i] << " ";
+    }
+    cout << endl;
+    return;
+}
+
+void difference2()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> sums(n+2,vector<int>(m+2,0));
+    vector<vector<int>> nums(n+2,vector<int>(m+2,0));
     for(int i=1;i<=n;++i)
     {
         for(int j=1;j<=m;++j)
@@ -15,7 +76,7 @@ void difference()
             nums[i][j]=sums[i][j]-sums[i-1][j]-sums[i][j-1]+sums[i-1][j-1];
         }
     }
-    int x1{},y1{},x2{},y2{},num{};
+    int x1,y1,x2,y2,num;
     cin >> x1 >> y1 >> x2 >> y2 >> num;
     nums[x1][y1]=nums[x1][y1]+num;
     nums[x1][y2+1]=nums[x1][y2+1]-num;
@@ -33,69 +94,12 @@ void difference()
     return;
 }
 
-void difference()
-{
-    int n{};
-    cin >> n;
-    int sums[n+2]{};
-    int nums[n+2]{};
-    for(int i=1;i<=n;++i)
-    {
-        cin >> sums[i];
-        nums[i]=sums[i]-sums[i-1];
-    }
-    int l{},r{},num{};
-    cin >> l >> r >> num;
-    nums[l]=nums[l]+num;
-    nums[r+1]=nums[r+1]-num;
-    for(int i=1;i<=n;++i)
-    {
-        sums[i]=sums[i-1]+nums[i];
-        cout << sums[i] << " ";
-    }
-    cout << endl;
-    return;
-}
-
-void prefix()
-{
-    int n{},m{};
-    cin >> n >> m;
-    int nums[n+1][m+1]{};
-    int sums[n+1][m+1]{};
-    for(int i=1;i<=n;++i)
-    {
-        for(int j=1;j<=m;++j)
-        {
-            cin >> nums[i][j];
-            sums[i][j]=nums[i][j]+sums[i-1][j]+sums[i][j-1]-sums[i-1][j-1];
-        }
-    }
-    int x1{},y1{},x2{},y2{};
-    cin >> x1 >> y1 >> x2 >> y2;
-    cout << sums[x2][y2]-sums[x1-1][y2]-sums[x2][y1-1]+sums[x1-1][y1-1]  << endl;
-    return;
-}
-
-void prefix()
-{
-    int n{};
-    cin >> n;
-    int nums[n+1]{};
-    int sums[n+1]{};
-    for(int i=1;i<=n;++i)
-    {
-        cin >> nums[i];
-        sums[i]=sums[i-1]+nums[i];
-    }
-    int l{},r{};
-    cin >> l >> r;
-    cout << sums[r]-sums[l-1] << endl;
-    return;
-}
-
 void solve()
 {
+    prefix1();
+    prefix2();
+    difference1();
+    difference2();
     return;
 }
 
