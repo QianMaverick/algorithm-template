fa7b75660d24056ba6cea2d78df44826bd1eea02
QianMaverick <schmieladamon7f@gmail.com>
2026-08-24 20:57:43 +0800

    Update c++prg.cpp

--- code/c++/random_number/c++prg.cpp
+++ code/c++/random_number/c++prg.cpp
@@ -1,23 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
+void solve1()
 {
     int a,b;
     cin >> a >> b;
-    static random_device seed;
-    static mt19937 engine(seed());
-    uniform_int_distribution<int> scope(a,b); // [a,b]
-    int num=scope(engine);
-    cout << num << endl;
-    uniform_real_distribution<double> scope(a,b); // [a,b)
-    double num=scope(engine);
-    cout << num << endl;
+    static random_device sed;
+    static mt19937 eng(sed());
+    uniform_int_distribution<int> scp(a,b);
+    cout << scp(eng) << endl;
+    return;
+}
+
+void solve2()
+{
+    double a,b;
+    cin >> a >> b;
+    static random_device sed;
+    static mt19937 eng(sed());
+    uniform_real_distribution<double> scp(a,b);
+    cout << scp(eng) << endl;
     return;
 }
 
 signed main()
 {
-    solve();
+    solve1();
+    solve2();
     return 0;
 }
